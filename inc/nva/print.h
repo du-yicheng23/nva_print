7eb45eb1a2564e166f0c043600b7d6bb7b5d0851
DuYicheng <duyicheng741@outlook.com>
2025-07-16 20:05:04 +0800

    增加 nva_print 函数

--- inc/nva/print.h
+++ inc/nva/print.h
@@ -2,12 +2,27 @@
  * @file print.h
  * @author DuYicheng
  * @date 2025-06-28
- * @brief 核心功能
+ * @brief 打印相关功能实现
  */
 
 #ifndef NVA_PRINT_H
 #define NVA_PRINT_H
 
+#include "nva/defines.h"
+#include "nva/format.h"
+
+NVA_EXTERN_C_BEGIN
+
+/**
+ * 用户自行实现的字符打印函数
+ * @param c 待打印的字符
+ * @retval 1 成功打印一个字符
+ * @retval 其他 打印失败
+ */
 int nva_putchar(char c);
+
+nva_ErrorCode nva_print(const char* NVA_RESTRICT format, nva_FmtStatus status);
+
+NVA_EXTERN_C_END
 
 #endif  // !NVA_PRINT_H
--- src/nva_print.c
+++ src/nva_print.c
@@ -2,7 +2,38 @@
  * @file nva_print.c
  * @author DuYicheng
  * @date 2025-06-28
- * @brief 核心功能
+ * @brief 打印相关功能实现
  */
 
+#include "nva/defines.h"
 #include "nva/print.h"
+#include "nva/format.h"
+
+#ifndef NVA_PRINT_BUFFER_SIZE
+#define NVA_BUFFER_SIZE 128
+#endif
+
+/**
+ * 打印字符串的缓冲区
+ */
+static char nva__print_buffer[NVA_BUFFER_SIZE];
+
+/**
+ * 格式化并打印字符串
+ * @param format 格式化字符串
+ * @param status 格式化状态
+ * @return nva_ErrorCode
+ */
+nva_ErrorCode nva_print(const char* NVA_RESTRICT format, const nva_FmtStatus status)
+{
+    const nva_ErrorCode ret = nva_format(nva__print_buffer, format, status);
+    NVA_SIZE_T i;
+
+    for (i = 0U; i < NVA_COUNTOF(nva__print_buffer) && nva__print_buffer[i] != '\0'; ++i) {
+        if (nva_putchar(nva__print_buffer[i]) != 1) {
+            return NVA_FAIL;
+        }
+    }
+
+    return ret;
+}
